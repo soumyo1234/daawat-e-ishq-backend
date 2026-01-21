ab74f392f43f2d949424a27b6b79e041d0e3face
tatay8317-lang <tatay8317@gmail.com>
2026-01-22 02:03:57 +0530

    Ensure Render-compatible server startup

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// server/server.js
-const app = require('./app');
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
